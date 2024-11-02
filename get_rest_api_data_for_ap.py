800a9297a9a080cb2833a16112a7d7faef024cde
Georg Hartner <georg.hartner@gmail.com>
2024-11-02 13:40:55 +0100

    optimise/debug python code

--- get_rest_api_data_for_ap.py
+++ get_rest_api_data_for_ap.py
@@ -11,6 +11,9 @@ connection_id = "test"
 country = "AT"
 coding_schema = "NAT"
 accounting_point_id = "AT0020000000000000000000100341061"
+from_dt = "2024-09-10T00:00:00.000000Z"
+to_dt = "2024-010-31T00:00:00.000000Z"
+tariffCode = "AWATTAR"
 
 data = {
     "grant_type": 'password',
@@ -34,7 +37,8 @@ if response.status_code == 200:
     print("Now using access token to request data from API:")
 
     access_token = response.json()["access_token"]
-    url = f'https://data-api.entarc.eu/timeseries/{connection_id}/{country}/{coding_schema}/{accounting_point_id}'
+    url = f'https://data-api.entarc.eu/timeseries/{connection_id}/{country}/{coding_schema}/{accounting_point_id}/{from_dt}/{to_dt}'
+    # url = f'https://data-api.entarc.eu/timeseriesWithPrice/{connection_id}/{country}/{coding_schema}/{accounting_point_id}/{from_dt}/{to_dt}/{tariffCode}'
 
     headers = {
         'Authorization': f'Bearer {access_token}'
